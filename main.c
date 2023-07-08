dd9c734628c1ca7a965b633f43bb6d8d605ff3c0
ssaammeet <136755038+ssaammeet@users.noreply.github.com>
2023-07-08 11:43:44 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float r ;
+    float pi=3.14159 ;
+
+    printf("Lutfen Dairenin Yaricapini Giriniz\n") ;
+    scanf("%f",&r) ;
+    printf("Dairenin capi %f\n",2*r) ;
+    printf("Dairenin Cevresi %f\n",2*pi*r) ;
+    printf("Dairenin Alani %f\n",pi*r*r) ;
+
+
+
+
+
+    return 0;
+}
